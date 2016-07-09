52c292c91c0613bdbbde501944968ebd4a4c0671
RedOrchid15 <sarahcseymour@gmail.com>
2016-07-09 11:41:54 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    document.getElementsByClassName("gwc-topnav")[0].classList.toggle("responsive");
+}
